d09af0d1c1435fd9d875675b81740658445078e4
IgorK <36182480+Goshaka@users.noreply.github.com>
2020-11-09 20:51:08 +0200

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -14,16 +14,16 @@ def home():
     return html.format(name=os.getenv("NAME", "health-test"))
 
 @app.route("/rest1")
-def rest1():
-    html = "<h3>Connecting to  {name1}!</h3>" \
-           "<b>Docker 1 hostname:</b> {hostname}<br/>"
-    return html.format(name1=os.getenv("NAME", "REST-API1"), hostname=socket.gethostname())
+# def rest1():
+#     html = "<h3>Connecting to  {name1}!</h3>" \
+#            "<b>Docker 1 hostname:</b> {hostname}<br/>"
+#     return html.format(name1=os.getenv("NAME", "REST-API1"), hostname=socket.gethostname())
 
-@app.route("/rest2")
-def rest2():
-    html = "<h3>Connecting {name2}!</h3>" \
-           "<b>Docker 1:</b> {now}<br/>"
-    return html.format(name2=os.getenv("NAME", "REST-API2"), now=datetime.now())
+# @app.route("/rest2")
+# def rest2():
+#     html = "<h3>Connecting {name2}!</h3>" \
+#            "<b>Docker 1:</b> {now}<br/>"
+#     return html.format(name2=os.getenv("NAME", "REST-API2"), now=datetime.now())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
