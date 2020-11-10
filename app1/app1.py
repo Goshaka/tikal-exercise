1bd34ade7124ba4ca47687b75e6bd5d1a77ea6ed
vagrant <vagrant@vagrant.vm>
2020-11-10 07:25:48 +0000

    Add pytest

--- app1/app1.py
+++ app1/app1.py
@@ -1,29 +1,11 @@
 from flask import Flask
-import os
-import socket
-from datetime import datetime
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-
+@app.route('/')
+def hello():
     html = "<h3>The system is up and running: {name}!</h3>" \
-           "<b>Docker 1</b> <br/>"
-    return html.format(name=os.getenv("NAME", "health-test"))
+           "<b>Docker App1</b> <br/>"
+    return 'Docker, App1!'
 
-@app.route("/rest1")
-# def rest1():
-#     html = "<h3>Connecting to  {name1}!</h3>" \
-#            "<b>Docker 1 hostname:</b> {hostname}<br/>"
-#     return html.format(name1=os.getenv("NAME", "REST-API1"), hostname=socket.gethostname())
-
-# @app.route("/rest2")
-# def rest2():
-#     html = "<h3>Connecting {name2}!</h3>" \
-#            "<b>Docker 1:</b> {now}<br/>"
-#     return html.format(name2=os.getenv("NAME", "REST-API2"), now=datetime.now())
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
--- app1/test_app1.py
+++ app1/test_app1.py
@@ -1,7 +1,7 @@
-# test_hello.py
 from app1 import app
 
-def test_app1():
+def test_hello():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
+    assert response.data == b'Docker, App1!'
